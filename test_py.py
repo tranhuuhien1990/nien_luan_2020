247bd2b0d534d90783f10534c96f6f1de44bc6f0
TranHuuHien <thhien@ctu.edu.vn>
2020-10-16 10:06:51 +0700

    test python

---
+++ test_py.py
@@ -0,0 +1,23 @@
+#Usage: py test_py.py 5
+
+import sys
+
+def fact(n):
+    """
+    Factorial function
+
+    :arg n: Number
+    :returns: factorial of n
+
+    """
+    if n == 0:
+        return 1
+    return n * fact(n -1)
+
+def main(n):
+    res = fact(n)
+    print(res)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        main(int(sys.argv[1]))
