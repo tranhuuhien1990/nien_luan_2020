841e59caa016f5047b235931cb0a615a0c277537
TranHuuHien <thhien@ctu.edu.vn>
2021-01-07 09:37:15 +0700

    ve biểu đồ từ csv

--- task/task_4_ve_bieu_do_tu_csv.py
+++ task/task_4_ve_bieu_do_tu_csv.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
-y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
-
 df = pd.read_csv('task3.csv')
+print(df.to_string())
+x = df.price
+y = df.rating
 
-plt.scatter(df.price, df.rating)
+
+plt.scatter(x, y)
 plt.show() 
